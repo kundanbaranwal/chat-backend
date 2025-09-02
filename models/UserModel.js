bc13577b83637cc0e3e8837add9811ff9407be46
Kundan baranwal <baranwalk624@gmail.com>
2025-09-02 11:05:21 +0530

    ok

--- models/UserModel.js
+++ models/UserModel.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-//! Hash user password before saving
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
